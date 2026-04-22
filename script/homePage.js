4e46ad53595abc8089dfe06c6812cdf4e1c524b1
virtualslug <victor13.ip1@gmail.com>
2026-04-22 16:10:05 -0300

    Adicionei JS na manipulação da caixa de mensagem de erro

--- script/homePage.js
+++ script/homePage.js
@@ -19,9 +19,40 @@ const limparCampos = ()=>{
     jsDataEntrada.value = '';
 }
 
+const verificandoCampos = ()=>{
 
-jsAdicionarBtn.addEventListener('click', ()=>{
-});
+    const message = 'Todos os campos precisam ser preenchidos';
+
+    const jsMessage = document.querySelector('.jsMessage');
+    const jsMessageBox = document.querySelector('.jsMessageBox');
+
+    const jsNomeProduto = document.querySelector('.jsNomeProduto');
+    const jsCategoria = document.querySelector('.jsCategoria');
+    const jsQuantidadeInicial = document.querySelector('.jsQuantidadeInicial');
+    const jsQuantidadeMinima = document.querySelector('.jsQuantidadeMinima');
+    const jsLocalizacao = document.querySelector('.jsLocalizacao');
+    const jsFornecedor = document.querySelector('.jsFornecedor');
+    const jsDataEntrada = document.querySelector('.jsDataEntrada');
+
+    if(jsNomeProduto.value.trim() === '' || jsCategoria.value.trim() === '' || jsQuantidadeInicial.value.trim() === '' || jsQuantidadeMinima.value.trim() === '' || jsLocalizacao.value.trim() === '' || jsFornecedor.value === '' || jsDataEntrada.value.trim() === ''){
+
+        jsMessageBox.classList.remove('hiden');
+        jsMessage.innerText = message;
+        return;
+    }
+}
+
+const closeMessageBox = ()=>{
+    const jsMessageBox = document.querySelector('.jsMessageBox');
+    jsMessageBox.classList.add('hiden');
+}
+
+const jsCloseError = document.querySelector('.jsCloseError');
+
+
+jsAdicionarBtn.addEventListener('click', verificandoCampos);
 
 jsLimparBtn.addEventListener('click', limparCampos);
+
+jsCloseError.addEventListener('click', closeMessageBox);
 
