b43e22519c99538cf864e4484a9afe707bd70a5a
virtualslug <victor13.ip1@gmail.com>
2026-05-04 13:10:20 -0300

    Reformulei o layout da página de estoque e coloquei ouvintes nos botões de entrada e saída de produto

--- script/estoquePage.js
+++ script/estoquePage.js
@@ -1,46 +1,66 @@
 import { produtos } from "../data/data.js";
 
-const jsTableContainer = document.querySelector('.jsTableContainer');
+const jsTableContainer = document.querySelector('.jsProdutosContainer');
 
-let produtosHTML = `
-<tr class="produtos-header cell-grid">
-                    <th class="header-name">NOME</th>
-                    <th class="header-quantity">QUANTIDADE</th>
-                    <th class="header-localizacao">LOCALIZAÇÃO</th>
-</tr>
-`;;
+
+let produtosHTML = ``;
 
 
 produtos.forEach((produto)=>{
 
    produtosHTML+= `
-   <tr class="produtos-box jsProdutoBox cell-grid" data-produto-id="${produto.id}">
-            <td class="nome-produto">${produto.nome}</td>
-            <td class="quantidade-produto">${produto.quantidade}</td>
-            <td class="prateleira-produto">${produto.localizacao}</td>
-  </tr>
+    <div class="produtos-box">
+                <a href="/produtoPage.html?produtoNome=${produto.nome}&produtoId=${produto.id}"><img id="page-icon" src="./assets/page-icon.svg" alt="page-icon"></a>
+                <div>Nome: <span class="nome-produto produto">${produto.nome}</span></div>
+                <div>Quantidade: <span class="quantidade-produto produto">${produto.quantidade}</span></div>
+                <input class="js-movimentacao-${produto.id} movimentacao" type="number">
+                <div class="buttons-box">
+                    <button class="jsEntradaBtt" data-produto-id="${produto.id}" id="entrada-produto">Entrada</button>
+                    <button class="jsSaidaBtt" data-produto-id="${produto.id}" id="saida-produto">Saída</button>
+                </div>
+            </div>
    `;
 });
 
 jsTableContainer.innerHTML = produtosHTML;
 
 
-const jsProdutoBox = document.querySelectorAll('.jsProdutoBox');
 
-jsProdutoBox.forEach((produtoBox)=>{
- produtoBox.addEventListener('click', ()=>{
 
-   let matchingProduct;
+function entradaProduto (id){
+   const valor = document.querySelector(`.js-movimentacao-${id}`);
 
-   const produtoId = produtoBox.dataset.produtoId;
+   console.log(valor.value);
+   
+   valor.value = '';
+}
 
-   produtos.forEach((produto)=>{
+function saidaProduto (id){
+   const valor = document.querySelector(`.js-movimentacao-${id}`);
 
-      if(produto.id === produtoId){
-         matchingProduct = produto;
-      }
-   });
+   console.log(valor.value);
 
-   window.location.href=`produtoPage.html?produtoNome=${matchingProduct.nome}&produtoId=${matchingProduct.id}`;
- })  
+   valor.value = '';
+}
+
+
+const jsEntradaBtt = document.querySelectorAll(".jsEntradaBtt");
+const jsSaidaBtt = document.querySelectorAll(".jsSaidaBtt");
+
+jsEntradaBtt.forEach((button)=>{
+
+   button.addEventListener('click', ()=>{
+      const buttonId = button.dataset.produtoId;
+
+      entradaProduto(buttonId);
+   })
+})
+
+jsSaidaBtt.forEach((button)=>{
+
+   button.addEventListener('click', ()=>{
+      const buttonId = button.dataset.produtoId;
+
+      saidaProduto(buttonId);
+   })
 })
