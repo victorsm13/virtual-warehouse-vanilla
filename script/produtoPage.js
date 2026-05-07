5b1420c38633fab68bad33c0fe7f81642ba4283a
virtualslug <victor13.ip1@gmail.com>
2026-05-07 10:22:23 -0300

    Adicionei ouvintes aos botões de editção na página dedicada de cada produto

--- script/produtoPage.js
+++ script/produtoPage.js
@@ -22,27 +22,27 @@ produtos.forEach((produto)=>{
 produtoHTML = `
         <img class="product-icon" src="./assets/product-icon.svg" alt="product-icon">
          <div class="product-informations-box">
-                <div>Nome: <span class="produto">${matchingProduct.nome}</span><input class="js-produto-nome hiden inputProduto" type="text"> <img class="js-edit-name icon" src="./assets/edit-icon-black.svg" alt=""> </div>
-                <div>Categoria: <span class="produto">${matchingProduct.categoria}</span><select class="js-categoria hiden inputProduto" name="cateforia" id="categoria">
+                <div>Nome: <span class="produto nome">${matchingProduct.nome}</span><input class="js-edit-nome hiden inputProduto" type="text"> <img class="icon" data-edit="nome" src="./assets/edit-icon-black.svg" alt=""> </div>
+                <div>Categoria: <span class="produto categoria">${matchingProduct.categoria}</span><select class="js-edit-categoria hiden inputProduto" name="cateforia" id="categoria">
                     <option value="escritorio">Materias de escritório</option>
                     <option value="TI">Equipamentos de TI</option>
                     <option value="eletrico">Materias elétricos</option>
                     <option value="Limpeza">Limpeza e manutenção</option>
                     <option value="producao">Materias de produção</option>
                     <option value="geral">Geral</option>
-                </select> <img class="icon" src="./assets/edit-icon-black.svg" alt=""></div>
-                <div>Quantidade: <span class="produto">${matchingProduct.quantidade}</span><input class="js-quantidade hiden inputProduto" type="number"> <img class="icon" src="./assets/edit-icon-black.svg" alt=""></div>
-                <div>Estoque Mínimo: <span class="produto">${matchingProduct.estoqueMinimo}</span><input class="js-estoque-minimo hiden inputProduto" type="number"> <img class="icon" src="./assets/edit-icon-black.svg" alt=""></div>
-                <div>Localização: <span class="produto">${matchingProduct.localizacao}</span><select class="jsLocalizacao hiden inputProduto" name="localizacao" id="localizacao">
+                </select> <img class=" icon" data-edit="categoria" src="./assets/edit-icon-black.svg" alt=""></div>
+                <div>Quantidade: <span class="produto quantidade">${matchingProduct.quantidade}</span><input class="js-edit-quantidade hiden inputProduto" data-edit="quantidade" type="number"> <img class=" icon" data-edit="quantidade" src="./assets/edit-icon-black.svg" alt=""></div>
+                <div>Estoque Mínimo: <span class="produto estoqueMinimo">${matchingProduct.estoqueMinimo}</span><input class="js-edit-estoqueMinimo hiden inputProduto" type="number"> <img class=" icon" data-edit="estoqueMinimo" src="./assets/edit-icon-black.svg" alt=""></div>
+                <div>Localização: <span class="produto prateleira">${matchingProduct.localizacao}</span><select class="js-edit-prateleira hiden inputProduto" name="localizacao" id="localizacao">
                     <option value="prateleiraUm">Prateleira 1</option>
                     <option value="prateleiraDois">Prateleria 2</option>
                     <option value="prateleiraTres">Prateleira 3</option>
                     <option value="prateleiraQuatro">Prateleira 4</option>
                     <option value="prateleiraCinco">Prateleira 5</option>
                     <option value="prateleiraSeis">Prateleira 6</option>
-                </select> <img class="icon" src="./assets/edit-icon-black.svg" alt=""></div>
-                <div>Fornecedor: <span class="produto">${matchingProduct.fornecedor}</span><input class="js-fornecedor hiden inputProduto" type="text"> <img class="icon" src="./assets/edit-icon-black.svg" alt=""></div>
-                <div>Data de entradada: <span class="produto">${matchingProduct.dataEntrada}</span><input class="js-data-produto hiden inputProduto" type="date"> <img class="icon" src="./assets/edit-icon-black.svg" alt=""></div>
+                </select> <img class="js-edit-prateleira-${matchingProduct.id} icon" data-edit="prateleira" src="./assets/edit-icon-black.svg" alt=""></div>
+                <div>Fornecedor: <span class="produto fornecedor">${matchingProduct.fornecedor}</span><input class="js-edit-fornecedor hiden inputProduto" type="text"> <img class="icon" data-edit="fornecedor" src="./assets/edit-icon-black.svg" alt=""></div>
+                <div>Data de entradada: <span class="produto dataEntrada">${matchingProduct.dataEntrada}</span><input class="js-edit-dataEntrada hiden inputProduto" type="date"> <img class=" icon" data-edit="dataEntrada" src="./assets/edit-icon-black.svg" alt=""></div>
                 <div>Última moviemntação: <span class="produto"></span></div>   
         </div>
         <div class="buttonsBox">
@@ -53,11 +53,54 @@ produtoHTML = `
         </div>
 `;
 
+function editarCampo(nome){
+
+    const jsInputEdit = document.querySelector(`.js-edit-${nome}`);
+    const jsSpanElement = document.querySelector(`.${nome}`);
+
+    if(nome === 'nome'){
+        jsSpanElement.classList.add('hiden');
+        jsInputEdit.classList.remove('hiden');
+        
+    } else if(nome === 'categoria'){
+        jsSpanElement.classList.add('hiden');
+        jsInputEdit.classList.remove('hiden');
+    } else if (nome === 'quantidade'){
+        jsSpanElement.classList.add('hiden');
+        jsInputEdit.classList.remove('hiden');
+    } else if (nome === 'estoqueMinimo'){
+         jsSpanElement.classList.add('hiden');
+        jsInputEdit.classList.remove('hiden');
+    } else if (nome === 'prateleira'){
+        jsSpanElement.classList.add('hiden');
+        jsInputEdit.classList.remove('hiden');
+    } else if (nome === 'fornecedor'){
+        jsSpanElement.classList.add('hiden');
+        jsInputEdit.classList.remove('hiden');
+    } else {
+        jsSpanElement.classList.add('hiden');
+        jsInputEdit.classList.remove('hiden');
+    }
+};
+
 jsProdutoBox.innerHTML = produtoHTML;
 
 const jsEditarBtn = document.querySelector('.js-editar-btn');
 
 const jsEditName = document.querySelector('.js-edit-name');
+
+const editBtns = document.querySelectorAll('.icon');
+
+editBtns.forEach((editBtn)=>{
+
+    editBtn.addEventListener('click', ()=>{
+
+        const buttonNome = editBtn.dataset.edit;
+
+        editarCampo(buttonNome);
+       }
+    );
+})
 
 
 jsEditarBtn.addEventListener('click', ()=>{
